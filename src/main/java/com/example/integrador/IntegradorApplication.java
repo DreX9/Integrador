3196143fd8129f3ce4d3456e0ba3eef6d9961e14
drex90 <drex.nada123@gmail.com>
2025-05-11 18:47:20 -0500

    conexion Base de datos

--- src/main/java/com/example/integrador/IntegradorApplication.java
+++ src/main/java/com/example/integrador/IntegradorApplication.java
@@ -1,7 +1,13 @@
 package com.example.integrador;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IntegradorApplication {
@@ -9,5 +15,14 @@ public class IntegradorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IntegradorApplication.class, args);
 	}
-
+	@Bean
+    public CommandLineRunner pruebaConexion(DataSource dataSource) {
+        return args -> {
+            try (Connection conn = dataSource.getConnection()) {
+                System.out.println("✅ Conexión exitosa a la base de datos: " + conn.getCatalog());
+            } catch (SQLException e) {
+                System.out.println("❌ Error de conexión: " + e.getMessage());
+            }
+        };
+    }
 }
