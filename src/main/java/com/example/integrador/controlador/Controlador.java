061b0f1eb791365907a46784a433f2b3bf4d488f
drex90 <drex.nada123@gmail.com>
2025-05-14 01:20:37 -0500

    ULTIMOS CAMBIOS

--- src/main/java/com/example/integrador/controlador/Controlador.java
+++ src/main/java/com/example/integrador/controlador/Controlador.java
@@ -14,12 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.integrador.interfacesService.IalmcenService;
 import com.example.integrador.interfacesService.IclasificacionService;
 import com.example.integrador.modelo.Almacen;
-<<<<<<< HEAD
+
 import com.example.integrador.modelo.Clasificacion;
-=======
+
 import jakarta.validation.Valid;
 
->>>>>>> d199c8a573a091375650332935e76946d665badd
+
 
 @Controller
 @RequestMapping
@@ -29,25 +29,25 @@ public class Controlador {
     private IalmcenService service;
     private IclasificacionService clasService;
 
-    @GetMapping
-    public String inicio () {
-        return "index";
-    }
+    // @GetMapping
+    // public String inicio () {
+    //     return "index";
+    // }
 
-    @GetMapping("/compras")
-    public String compras () {
-        return "compras";
-    }
+    // @GetMapping("/compras")
+    // public String compras () {
+    //     return "compras";
+    // }
 
-    @GetMapping("/ventas")
-    public String ventas () {
-        return "ventas";
-    }
+    // @GetMapping("/ventas")
+    // public String ventas () {
+    //     return "ventas";
+    // }
 
-    @GetMapping("/productos")
-    public String productos () {
-        return "productos";
-    }
+    // @GetMapping("/productos")
+    // public String productos () {
+    //     return "productos";
+    // }
 
     @GetMapping("/clasificaciones")
     public String clasificaciones (Model model) {
@@ -63,47 +63,46 @@ public class Controlador {
         model.addAttribute("almacen", new Almacen());
         return "almacenes";
     }
-<<<<<<< HEAD
 
-    @GetMapping("/proveedores")
-    public String proveedores () {
-        return "proveedores";
-    }
 
-    @GetMapping("/clientes")
-    public String clientes () {
-        return "clientes";
-    }
+    // @GetMapping("/proveedores")
+    // public String proveedores () {
+    //     return "proveedores";
+    // }
 
-    @GetMapping("/empleados")
-    public String empleados () {
-        return "empleados";
-    }
+    // @GetMapping("/clientes")
+    // public String clientes () {
+    //     return "clientes";
+    // }
 
-    @GetMapping("/devolucion-compra")
-    public String devolucionCompra () {
-        return "devolucion-compra";
-    }
+    // @GetMapping("/empleados")
+    // public String empleados () {
+    //     return "empleados";
+    // }
 
-    @GetMapping("/devolucion-venta")
-    public String devolucionVenta () {
-        return "devolucion-venta";
-    }
+    // @GetMapping("/devolucion-compra")
+    // public String devolucionCompra () {
+    //     return "devolucion-compra";
+    // }
+
+    // @GetMapping("/devolucion-venta")
+    // public String devolucionVenta () {
+    //     return "devolucion-venta";
+    // }
     
-    @GetMapping("/inventarios")
-    public String inventarios () {
-        return "inventarios";
-    }
+    // @GetMapping("/inventarios")
+    // public String inventarios () {
+    //     return "inventarios";
+    // }
 
-    @GetMapping("/reportes")
-    public String reportes () {
-        return "reportes";
-=======
+    // @GetMapping("/reportes")
+    // public String reportes () {
+    //     return "reportes";
+    // }
     @PostMapping("/save") 
     public String save(@Valid  Almacen a, Model model) {
         service.Save(a);
         return "redirect:/almacenes";
->>>>>>> d199c8a573a091375650332935e76946d665badd
     }
     @GetMapping("/editar/{id_almacen}")
     public String editar(@PathVariable int id_almacen, Model model){
