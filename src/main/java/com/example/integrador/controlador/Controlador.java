93959abfe8ff510d43365fa2e5b3a486c4479fa9
drex90 <drex.nada123@gmail.com>
2025-05-13 23:21:09 -0500

    guardatosAlmacen

--- src/main/java/com/example/integrador/controlador/Controlador.java
+++ src/main/java/com/example/integrador/controlador/Controlador.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.integrador.interfacesService.IalmcenService;
 import com.example.integrador.modelo.Almacen;
+import jakarta.validation.Valid;
+
 
 @Controller
 @RequestMapping
@@ -22,6 +25,12 @@ public class Controlador {
     public String listar(Model model) {
         List<Almacen>almacenes=service.listar();
         model.addAttribute("almacenes", almacenes);
+        model.addAttribute("almacen", new Almacen());
         return "almacenes";
+    }
+    @PostMapping("/save") 
+    public String save(@Valid  Almacen a, Model model) {
+        service.Save(a);
+        return "redirect:/almacenes";
     }
 }
