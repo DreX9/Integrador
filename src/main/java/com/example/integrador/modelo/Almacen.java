60edc2ef295202bb286dda853380d30e68dca848
drex90 <drex.nada123@gmail.com>
2025-05-13 22:05:50 -0500

    ultima actualizacion

--- src/main/java/com/example/integrador/modelo/Almacen.java
+++ src/main/java/com/example/integrador/modelo/Almacen.java
@@ -7,27 +7,29 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "almacen")
+@Table(name = "almacenes")
 public class Almacen {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer id_almacen;
     private String nombre;
     private String descripcion;
+    private String direccion;
     public Almacen() {
 
     }
-    public Almacen(Integer id, String nombre, String descripcion) {
+    public Almacen(Integer id_almacen, String nombre, String direccion, String descripcion) {
         super();
-        this.id = id;
+        this.id_almacen = id_almacen;
         this.nombre = nombre;
         this.descripcion = descripcion;
+        this.direccion = direccion;
     }
     public String getDescripcion() {
         return descripcion;
     }
-    public Integer getId() {
-        return id;
+    public Integer getId_almacen() {
+        return id_almacen;
     }
     public String getNombre() {
         return nombre;
@@ -35,10 +37,16 @@ public class Almacen {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-    public void setId(Integer id) {
-        this.id = id;
+    public void setId_almacen(Integer id_almacen) {
+        this.id_almacen = id_almacen;
     }
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
     }
 }
